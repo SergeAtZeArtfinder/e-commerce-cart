8e4425263d6d9f9e758a3300d042255549cea4ff
Serge Basangovs <sergejs.basangovs@artfinder.com>
2024-12-03 16:17:10 +0000

    feat: product details page

---
+++ src/app/not-found.tsx
@@ -0,0 +1,11 @@
+const NotFoundPage = () => {
+  return (
+    <>
+      <h1 className="text-3xl font-bold underline text-center">
+        Page not found
+      </h1>
+    </>
+  )
+}
+
+export default NotFoundPage
---
+++ src/app/products/[id]/page.tsx
@@ -0,0 +1,67 @@
+import { notFound } from 'next/navigation'
+import { unstable_cache as cache } from 'next/cache'
+import Image from 'next/image'
+
+import type { Metadata } from 'next'
+import type { PageProps } from '@/types'
+
+import prisma from '@/lib/db/prisma'
+import PriceTag from '@/components/PriceTag'
+
+const getProduct = cache(async (id: string) => {
+  const product = await prisma.product.findUnique({
+    where: { id },
+  })
+
+  if (!product) {
+    return notFound()
+  }
+
+  return product
+})
+
+export const generateMetadata = async ({
+  params,
+}: PageProps<{ id: string }>): Promise<Metadata> => {
+  const product = await getProduct(params.id)
+  return {
+    title: product.name + ' - Flowmazon',
+    description: product.description,
+    openGraph: {
+      type: 'website',
+      title: product.name,
+      description: product.description,
+      images: [
+        {
+          url: product.imageUrl,
+        },
+      ],
+    },
+  }
+}
+
+const ProductDetailsPage = async ({
+  params: { id },
+}: PageProps<{ id: string }>) => {
+  const product = await getProduct(id)
+
+  return (
+    <div className="flex flex-col lg:flex-row lg:items-center gap-4">
+      <Image
+        src={product.imageUrl}
+        alt={product.name}
+        width={500}
+        height={500}
+        className="rounded-lg"
+        priority
+      />
+      <div>
+        <h1 className="text-5xl font-bold">{product.name}</h1>
+        <PriceTag price={product.price} className="mt-4" />
+        <p className="py-4">{product.description}</p>
+      </div>
+    </div>
+  )
+}
+
+export default ProductDetailsPage
